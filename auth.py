fc4214faf2cccb86411563e13219367117b41686
AF <gulabsinghrajput008@gmail.com>
2024-12-26 16:17:42 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,6 +2,6 @@ import os
 
 
 class Vauth:
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("BOT_TOKEN" , "7793828619:AAHnY60vTcElNV_Trmd_DE6pKi7y_RKqRO8")
+    API_ID = int(os.getenv("API_ID", "26494161"))
+    API_HASH = os.getenv("API_HASH" , "55da841f877d16a3a806169f3c5153d3")
